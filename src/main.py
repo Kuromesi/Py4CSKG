9d173f57ba6e4b35bcac3d22603b8f5c2c61ccc0
Kuromesi <blackfacepan@163.com>
2022-09-30 16:21:06 +0800

    first commit

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ nlp = spacy.load('./data/v1\output\model-best')
 txt = "vRealize Operations (7.x before 7.0.0.11287810, 6.7.x before 6.7.0.11286837 and 6.6.x before 6.6.1.11286876) contains a local privilege escalation vulnerability due to improper permissions of support scripts"
 tmp = nlp(txt)
 for ent in tmp.ents:
-        print(ent.text, ent.label_)W
+        print(ent.text, ent.label_)
